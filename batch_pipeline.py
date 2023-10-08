c04733d0a623e0ce51d6773f12c54acd8b5f27ba
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-10-08 23:50:12 +0000

    read in every line of csv

--- batch_pipeline.py
+++ batch_pipeline.py
@@ -1,14 +1,35 @@
 import argparse
 import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
+import csv
+
+
+# import pprint
+# pp = pprint.PrettyPrinter(indent=4)
+
 
 def run(argv=None, save_main_session=True):
-    # Code for parsing arguments was copied from https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/wordcount.py
+    # Code for parsing command line arguments was copied from https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/wordcount.py
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+      '--input',
+      dest='input',
+      help='Input file to process.')
+    parser.add_argument(
+      '--output',
+      dest='output',
+      required=True,
+      help='Output file to write results to.')
     known_args, pipeline_args = parser.parse_known_args(argv)
     pipeline_options = PipelineOptions(pipeline_args)
     pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        rows = p | 'ReadCSV' >> ReadFromText(known_args.input)
+        rows | 'Write' >> WriteToText(known_args.output)
 
 
 
