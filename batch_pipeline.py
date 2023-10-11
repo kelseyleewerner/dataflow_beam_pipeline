fd641129139877416ffe91b09d8445c9188a9617
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-10-11 01:39:18 +0000

    finalize using Beam's Create method to import CSV to a PCollection where each item is a dictionary of column keys with row values

--- batch_pipeline.py
+++ batch_pipeline.py
@@ -1,21 +1,23 @@
 import apache_beam as beam
-from apache_beam.dataframe.convert import to_dataframe, to_pcollection
 from apache_beam.dataframe.io import read_csv
-from apache_beam.io import ReadFromText, WriteToText
+from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
 import argparse
-
-
-import csv
 import codecs
-
-from apache_beam.dataframe.convert import to_pcollection
-
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
+import csv
 
 
 
+# import pprint
+# pp = pprint.PrettyPrinter(indent=4)
+
+
+
+# Code for reading in a CSV from a Cloud Storage bucket as text and return an iterable that can be consumed by 
+#   a Beam PTransform was copied from the following sources:
+# https://stackoverflow.com/questions/68215269/how-to-handle-newlines-when-loading-a-csv-into-apache-beam
+# https://stackoverflow.com/questions/58500594/open-file-in-beam-io-filebasedsource-issue-with-python-3
+# https://stackoverflow.com/questions/29383475/how-to-create-dict-using-csv-file-with-first-row-as-keys
 def read_csv_file(file_path):
   with beam.io.filesystems.FileSystems.open(file_path, mime_type='text/plain') as infile:
     for row in csv.DictReader(codecs.iterdecode(infile, "utf-8")):
@@ -24,7 +26,6 @@ def read_csv_file(file_path):
 def run(argv=None, save_main_session=True):
     # Code for parsing command line arguments was copied from https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/wordcount.py
     parser = argparse.ArgumentParser()
-    
     parser.add_argument(
       '--input',
       dest='input',
@@ -40,18 +41,8 @@ def run(argv=None, save_main_session=True):
 
     with beam.Pipeline(options=pipeline_options) as p:
         rows = p | 'ReadCSV' >> beam.Create(read_csv_file(known_args.input))
-
-
         
-        # rows = p | 'ReadCSV' >> read_csv(known_args.input)
-
-        # rows_as_pcoll = to_pcollection(rows)
-
-        # pp.pprint(type(rows))
-
-        # rows | 'Type' >> beam.Map(lambda x: type(x))
-
-        # rows = p | 'ReadCSV' >> ReadFromText(known_args.input)
+        
         rows | 'Write' >> WriteToText(known_args.output)
 
 
