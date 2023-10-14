6e7faa5a81482cc98a527ff4974a10ae6e5af4c7
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-10-14 03:37:35 +0000

    filter out unnecessary keys

--- batch_pipeline.py
+++ batch_pipeline.py
@@ -1,5 +1,4 @@
 import apache_beam as beam
-from apache_beam.dataframe.io import read_csv
 from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
 import argparse
@@ -23,29 +22,64 @@ def read_csv_file(file_path):
     for row in csv.DictReader(codecs.iterdecode(infile, "utf-8")):
         yield row
 
+class FilterFlightDataDoFn(beam.DoFn):
+    def process(self, element):
+        filtered_element = {
+            'age': element['Age'],
+            'departure_date': element['Departure Date'],
+            'arrival_airport': element['Arrival Airport'],
+            'flight_status': element['Flight Status']
+        }
+        return([filtered_element])
+
+
 def run(argv=None, save_main_session=True):
     # Code for parsing command line arguments was copied from https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/wordcount.py
     parser = argparse.ArgumentParser()
     parser.add_argument(
       '--input',
       dest='input',
+      required=True,
       help='Input file to process.')
     parser.add_argument(
       '--output',
       dest='output',
       required=True,
       help='Output file to write results to.')
+    # Date range defaults to 09/01/2022 - 12/29/2022 if not given as command line arguments
+    # Validating the --start and --end command line arguments provided from the user is left as a future enhancement
+    parser.add_argument(
+      '--start',
+      dest='start_date',
+      default='',
+      required=True,
+    #   TODO: fill in format info
+      help='Date range cannot start earlier than Dec 31, 2021 and must be in the format FORMAT1 or FORMAT2.')
+    parser.add_argument(
+      '--end',
+      dest='end_date',
+      required=True,
+      #   TODO: fill in format info
+      help='Date range cannot end after Dec 29, 2022 and must be in the format FORMAT1 or FORMAT2.')
     known_args, pipeline_args = parser.parse_known_args(argv)
     pipeline_options = PipelineOptions(pipeline_args)
     pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
+    # Start moving data through beam pipeline
     with beam.Pipeline(options=pipeline_options) as p:
         rows = p | 'ReadCSV' >> beam.Create(read_csv_file(known_args.input))
         
         
-        rows | 'Write' >> WriteToText(known_args.output)
+        # rows | 'Write' >> WriteToText(known_args.output)
 
+        output = (
+            rows 
+                | 'FilterFlightData' >> beam.ParDo(FilterFlightDataDoFn())
+            )
+
+        output | 'Write' >> WriteToText(known_args.output)
 
 
 if __name__ == '__main__':
     run()
+    
